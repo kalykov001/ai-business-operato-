1e0891f9cbff9bf83fa2339b2c60a4ef46f6d211
kalykov001 <kalykovsln@gmail.com>
2026-08-24 19:07:39 +0600

    fix production cors and api urls

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import createApi from "./createApi";
 import { env } from "./config/env";
+const port = Number(process.env.PORT) || 5000;
 const app = createApi();
-app.listen(env.port, () => {
-  console.log(`Server is running on http://localhost:${env.port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
